37340d6c9fbed849f69a9a8fcebc48b556b2c92e
naylafn <naylafarah.31@gmail.com>
2024-10-23 15:47:19 +0700

    product and review models

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from main.views import index
+from main.views import index, show_product, create_product, product_detail
 
 urlpatterns = [
     path('', index),
+    path('products/', show_product, name='show_product'),
+    path('create_product/', create_product, name='create_product'),
+    path('product/<uuid:product_id>/', product_detail, name='product_detail'),
 ]
