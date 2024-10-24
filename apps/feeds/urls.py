d82929cc5992040c59fda928a6f87eb8a2f0e00d
Hyvos <daniel.liman07@gmail.com>
2024-10-25 03:26:27 +0700

    feat: Add post + new design

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -5,6 +5,7 @@ app_name = 'feeds'
 
 urlpatterns = [
     path('', show_all, name='show_all'),
+    path('tabs/<str:all_tabs>/', change_tabs, name='change_tabs'),
     path('create/', create_post, name='create_post'),
     path('json/', json, name='json'),
     path('json/<str:id>/', json_by_id, name='json_by_id'),
