cde5331e767b7f916682f505776537261ff4179a
Hyvos <daniel.liman07@gmail.com>
2024-10-26 03:31:47 +0700

    feat: Forum Post and Comment (Likes Coming Soon!)

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -3,10 +3,22 @@ from apps.feeds.views import *
 
 app_name = 'feeds'
 
+# TODO: Likes Endpoint for Post and Reply
+
 urlpatterns = [
     path('', show_all, name='show_all'),
+    path('post/<uuid:id>/', detail_post, name='detail_post'),
     path('tabs/<str:all_tabs>/', change_tabs, name='change_tabs'),
-    path('create/', create_post, name='create_post'),
-    path('json/', json, name='json'),
-    path('json/<str:id>/', json_by_id, name='json_by_id'),
+    
+    # Posts
+    path('create/post/', create_post, name='create_post'),
+    path('json/post/', post_json, name='post_json'),
+    path('json/post/user/', post_json_by_user, name='post_json_by_user'), # TBD
+    path('json/post/id/<uuid:id>/', post_json_by_id, name='post_json_by_id'),
+    
+    # Replies
+    path('create/reply/', create_reply, name='create_reply'),
+    path('json/reply/<uuid:post_id>', reply_json, name='reply_json'),
+    path('json/reply/user/', reply_json_by_user, name='reply_json_by_user'), # TBD
+    path('json/reply/id/<uuid:id>/', reply_json_by_id, name='reply_json_by_id'),
 ]
