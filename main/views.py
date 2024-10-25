33bb6e4b92387c6060562bb33df82e0020bd5174
Kukuh Cikal <71339508+Furizou@users.noreply.github.com>
2024-10-25 11:44:20 +0700

    feat: authentication page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 
 def index(request):
     context = {
         'current_time': datetime.now(),
     }
     return render(request, "main.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
