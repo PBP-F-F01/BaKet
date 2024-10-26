3a27bf2bc67cea5e17b18a10681712db06fa8cd2
yasin <yasinmahendra26@gmail.com>
2024-10-26 10:28:45 +0700

    build: change the models of articles

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -4,19 +4,24 @@ import uuid
 
 class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=256)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    source = models.URLField(null=True, blank=True)
+    like_count = 0
+    comment_count = 0
 
-    @property
-    def like_count(self):
-        return Like.objects.filter(article=self).count()
-    
-    @property
-    def comment_count(self):
-        return Comment.objects.filter(article=self).count()
+    def inc_like(self):
+        self.like_count += 1
+
+    def dec_like(self):
+        self.like_count -= 1
+
+    def inc_comment(self):
+        self.comment_count += 1
+
+    def dec_comment(self):
+        self.comment_count -= 1
 
     class Meta:
         ordering = ['-created_at']
@@ -29,10 +34,13 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     has_edited = models.BooleanField(default=False)
+    like_count = 0
 
-    @property
-    def like_count(self):
-        return Like.objects.filter(article=self).count()
+    def inc_like(self):
+        self.like_count += 1
+
+    def dec_like(self):
+        self.like_count -= 1
     
     class Meta:
         ordering = ['-created_at']
@@ -42,4 +50,3 @@ class Like(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
