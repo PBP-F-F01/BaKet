33cbdebd43bdabc505ebcc1a531bbf1abfe8ece6
yasin <yasinmahendra26@gmail.com>
2024-10-27 17:33:44 +0700

    feat: compatible with footer

---
+++ static/js/articles/article.js
@@ -0,0 +1,13 @@
+// var article;
+// var user;
+// var comment;
+// function getArticleUserComment(a, u, c) {
+//     article = a;
+//     user = u;
+//     comment = c;
+//     pr();
+// }
+// function pr() {
+//     console.log(article);
+//     console.log(user);
+// }
