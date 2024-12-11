e7905b6ca13a19f766e0aa3190d77a758495f821
Kukuh Cikal <71339508+Furizou@users.noreply.github.com>
2024-12-11 23:14:39 +0700

    feat: UserProfile flutter integration

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from apps.authentication.views import login, register
+from apps.authentication.views import login, register, logout
 
 app_name = 'apps.authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
-
+    path('logout/', logout, name='logout'),
 ]
